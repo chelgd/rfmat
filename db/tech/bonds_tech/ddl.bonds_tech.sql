daa669cca9f412b217d1bc0ffdf9a114d42f60f8
Georgy Chelidze <chelgeorge@mail.ru>
2020-04-14 19:34:39 +0300

    Added delete and insert dml scripts

--- db/tech/bonds_tech/ddl.bonds_tech.sql
+++ db/tech/bonds_tech/ddl.bonds_tech.sql
@@ -2,7 +2,7 @@ CREATE TABLE tech.bonds_tech (
 	bonds_id int8 NULL,
 	src_id text NULL,
 	hash text NULL,
-	sytsem_name text NULL
-	dml_type text NULL
+	sytsem_name text null,
+	dml_type text null,
 	update_dttm timestamp NULL
 );
--- db/tech/bonds_tech/dml.bonds_tech.sql
+++ db/tech/bonds_tech/dml.bonds_tech.sql
@@ -1,9 +1,11 @@
 DO $do$
 BEGIN
 
-----------------------Обновлённые версионные данны---------------
+----------------------Обновлённые версионные данны----------------------
 
-	update test_tech.bonds_tech a
+----------------------Обновляем записи у обновлённых экземпляров сущности----------------------
+
+	update tech.bonds_tech a
 	
 	set hash = b.ods_hash,
 	dml_type = 'U',
@@ -17,8 +19,8 @@ BEGIN
 			md5 (a.type         ||
 			a.nominal          ||
 			a.coupon_amount    ||
-			a.coup_paym_date   ||
-			a.exp_date) as ods_hash 
+			to_date(a.coup_paym_date, 'dd.mm.yyyy')   ||
+			to_date(a.exp_date, 'dd.mm.yyyy')) as ods_hash 
 		from ods.bonds a )
 		
 		select 
@@ -26,7 +28,7 @@ BEGIN
 			a.ods_hash
 		from base a 
 		
-		inner join test_tech.bonds_tech b 
+		inner join tech.bonds_tech b 
 		on a.name = b.src_id 
 		
 		where a.ods_hash <> b.hash
@@ -34,7 +36,56 @@ BEGIN
 	) b
 	
 	where a.src_id = b.src_id
+	
 	;
+	
+----------------------Обновляем записи у удалённых экземпляров сущности----------------------
+
+	update tech.bonds_tech a
+	
+	set dml_type = 'D',
+	update_dttm = now()
+
+	from (
+		select 
+			a.src_id
+		from tech.bonds_tech a
+		
+		left join ods.bonds b 
+		on a.src_id = b.name
+		
+		where b.name is null 
+	) b
+	
+	where a.src_id = b.src_id
+	
+	;
+ 
+----------------------Добавление новых экземпляров сущности---------------------- 
+	insert into tech.bonds_tech
+	
+	select 
+		null as bond_id,
+		a.name as src_id,
+		md5 (a.type         ||
+		a.nominal          ||
+		a.coupon_amount    ||
+		to_date(a.coup_paym_date, 'dd.mm.yyyy')   ||
+		to_date(a.exp_date, 'dd.mm.yyyy')) as hash,
+		'quik' as system_name,
+		'I' as dml_type,
+		now() as update_dttm
+	
+	from ods.bonds a
+	
+	left join tech.bonds_tech b 
+	on a.name = b.src_id 
+	
+	where b.src_id is null
+	
+	;	
+ 
+ 
  
 END;
 $do$ 
