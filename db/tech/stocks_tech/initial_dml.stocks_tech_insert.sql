939d1b40f2732a39eaaac64488165a735e6112ac
agisyr <agisenov_jun@mail.ru>
2020-04-16 12:43:58 +0400

    new files: dml.stocks_v, initial_dml.stocks_v, dml_stocks_tech, initial_dml.stocks_tech; modified: new columns in stocks_tech, bonds_id >> bond_id, text >> date

---
+++ db/tech/stocks_tech/initial_dml.stocks_tech_insert.sql
@@ -0,0 +1,14 @@
+insert into tech.stocks_tech 
+select 
+stocks_id,
+name as src_id,
+md5(tool_code  ||
+lot            ||
+class_code) as hash,
+'quik' as system_name,
+'I' as dml_type,
+now() as update_dttm
+
+from dds.stocks_v
+
+
