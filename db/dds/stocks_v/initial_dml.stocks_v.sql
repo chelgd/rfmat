1adddf3e0d4f2e6032afcfe52b35996f2bc65031
Georgy Chelidze <chelgeorge@mail.ru>
2020-04-28 19:35:21 +0300

    Fixed initial_dml.stocks_v.sql

--- db/dds/stocks_v/initial_dml.stocks_v.sql
+++ db/dds/stocks_v/initial_dml.stocks_v.sql
@@ -1,13 +1,20 @@
-insert into dds.bonds_v
-select
-row_number () over () as bond_id,
-name,
-tool_code,
-lot,
-class_code,
-date(now()) as valid_from_dt,
-date('5999-01-01') as valid_to_dt,
-'I' as dml,
-1 as act_flg
-from ods.bonds
-;
+DO $do$
+BEGIN
+
+	insert into dds.bonds_v
+	select
+		row_number () over () as bond_id,
+		name,
+		tool_code,
+		lot,
+		class_code,
+		date(now()) as valid_from_dt,
+		date('5999-01-01') as valid_to_dt,
+		'I' as dml,
+		1 as act_flg
+	from ods.quik_stocks
+	;
+
+ 
+END;
+$do$ 	
