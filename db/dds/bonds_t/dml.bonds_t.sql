c269ec58dec842913d449378df68d967c83eed94
Georgy Chelidze <chelgeorge@mail.ru>
2020-04-29 18:52:21 +0300

    Added dml.bonds_t.sql

---
+++ db/dds/bonds_t/dml.bonds_t.sql
@@ -0,0 +1,20 @@
+DO $do$
+BEGIN
+
+	insert into dds.bonds_t 
+	select
+		b.bond_id,
+		a.lst_deal_price, 
+		a.weighted_price, 
+		a.prc_change, 
+		now() as transaction_dttm 	
+	from ods.quik_bonds a
+	
+	left join tech.bonds_tech b
+	on a.isin = b.src_id
+
+
+	;
+ 
+END;
+$do$ 
