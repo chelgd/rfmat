e28ca92785e14cb960c8720d15423e3364d66d04
Georgy Chelidze <chelgeorge@mail.ru>
2020-05-20 13:06:32 +0300

    Deleted wrong tech folders

--- db/tech/bonds_tech/ddl.bonds_tech.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tech.bonds_tech (
-	bond_id int8 NULL,
-	src_id text NULL,
-	hash text NULL,
-	sytsem_name text null,
-	dml text null,
-	update_dttm timestamp NULL
-);
--- db/tech/stocks_tech/ddl.stocks_tech.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tech.stocks_tech (
-	stocks_id int8 NULL,
-	src_id text NULL,
-	hash text NULL,
-	sytsem_name text NULL,
-	dml text null,
-	update_dttm timestamp NULL
-);
